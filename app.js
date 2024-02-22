7b7cf74f9204d7763c765e1f2d32e99fa1c57ce0
Rabin chhatkuli <140541319+Rabinxt@users.noreply.github.com>
2024-02-22 17:01:16 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let UserChoiceDisplay = document.getElementById('User Choice');
+let ComputerChoiceDisplay = document.getElementById('Computer Choice');
+let ResultDisplay= document.getElementById('Result');
+const PossibleChoices=document.querySelectorAll('button');
+let UserChoice ;
+let ComputerChoice;
+
+PossibleChoices.forEach(PossibleChoice => PossibleChoice.addEventListener('click' , e =>{
+    UserChoice= e.target.id;
+    UserChoiceDisplay.innerHTML = UserChoice;
+
+    generateComputerChoice();
+    Giveresult();
+}));
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * PossibleChoices.length);
+    if(randomNumber === 1){
+        ComputerChoice = "Rock" ;
+    }
+    if(randomNumber === 2){
+        ComputerChoice = "Sissors" ;
+    }
+    if(randomNumber === 0){
+        ComputerChoice = "Paper" ;
+    }
+
+    ComputerChoiceDisplay.innerHTML = ComputerChoice ;
+}
+
+function Giveresult(){
+    if(UserChoice === ComputerChoice){
+        Result = "It's a Draw!" ; 
+    }
+    if(UserChoice === 'Rock' && ComputerChoice === 'Paper'){
+        Result = "You Lost !" ; 
+    }
+    if(UserChoice === 'Rock' && ComputerChoice === 'Sissor'){
+        Result = "You Won!" ; 
+    }
+    if(UserChoice === 'Paper' && ComputerChoice === 'Rock'){
+        Result = "You Won!" ; 
+    }
+    if(UserChoice === 'Paper' && ComputerChoice === 'Sissors'){
+        Result = "You Lost !" ; 
+    }
+    if(UserChoice === 'Sissors' && ComputerChoice === 'Rock'){
+        Result = "You Lost !" ; 
+    }
+    if(UserChoice === 'Sissors' && ComputerChoice === 'Paper'){
+        Result = "You Won!" ; 
+    }
+    ResultDisplay.innerHTML = Result;
+}
