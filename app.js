9d7c0c57bf5bd813677df7269c57cc25c7c486b6
Rabinxt <brodebid@gmail.com>
2024-06-25 20:24:42 +0545

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const UserChoiceImg = document.getElementById('UserChoiceImg');
 const ComputerChoiceDisplay = document.getElementById('ComputerChoice');
@@ -64,59 +63,3 @@ resetButton.addEventListener('click', function() {
     UserChoice = null;
     ComputerChoice = null;
 });
-=======
-let UserChoiceDisplay = document.getElementById('User Choice');
-let ComputerChoiceDisplay = document.getElementById('Computer Choice');
-let ResultDisplay= document.getElementById('Result');
-const PossibleChoices=document.querySelectorAll('button');
-let UserChoice ;
-let ComputerChoice;
-
-PossibleChoices.forEach(PossibleChoice => PossibleChoice.addEventListener('click' , e =>{
-    UserChoice= e.target.id;
-    UserChoiceDisplay.innerHTML = UserChoice;
-
-    generateComputerChoice();
-    Giveresult();
-}));
-
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * PossibleChoices.length);
-    if(randomNumber === 0){
-        ComputerChoice = "Rock" ;
-    }
-    if(randomNumber === 1){
-        ComputerChoice = "Sissors" ;
-    }
-    if(randomNumber === 2){
-        ComputerChoice = "Paper" ;
-    }
-
-    ComputerChoiceDisplay.innerHTML = ComputerChoice ;
-}
-
-function Giveresult(){
-    if(UserChoice === ComputerChoice){
-        Result = "It's a Draw!" ; 
-    }
-    if(UserChoice === 'Rock' && ComputerChoice === 'Paper'){
-        Result = "You Lost !" ; 
-    }
-    if(UserChoice === 'Rock' && ComputerChoice === 'Sissor'){
-        Result = "You Won!" ; 
-    }
-    if(UserChoice === 'Paper' && ComputerChoice === 'Rock'){
-        Result = "You Won!" ; 
-    }
-    if(UserChoice === 'Paper' && ComputerChoice === 'Sissors'){
-        Result = "You Lost !" ; 
-    }
-    if(UserChoice === 'Sissors' && ComputerChoice === 'Rock'){
-        Result = "You Lost !" ; 
-    }
-    if(UserChoice === 'Sissors' && ComputerChoice === 'Paper'){
-        Result = "You Won!" ; 
-    }
-    ResultDisplay.innerHTML = Result;
-}
->>>>>>> 26b750463cc04b3db13e32d2f543ed956b26381a
